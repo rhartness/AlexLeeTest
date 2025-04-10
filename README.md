cc9ad43b42a642f82fc9b55603b9ba0684d6ee92
Richard Hartness <richard.hartness@clear-measure.com>
2025-04-10 16:48:58 -0400

    Fixing up build and run issues with the ReactJS application.

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # AlexLeeTest
 This repo consists of two, main projects.
 
- * The ReactJS application (provided for questions 7 & 8) is located in ui\alex-lee-app
- * The C# solution, containing all C#/MVC answers is located in the src directory.
+* The ReactJS application (provided for questions 7 & 8) is located in ui\alex-lee-app
+  * **PLEASE NOTE** that there seems to be a bug within the npx Create React App generator. I could not get the lighteningcss web module to install via any form of `npm install` command.  However, if you install yarn and run `yarn add lightningcss`, it should install the application and allow it to properly run.
+* The C# solution, containing all C#/MVC answers is located in the src directory.
--- ui/alex-lee-app/app/layout.js
+++ ui/alex-lee-app/app/layout.js
@@ -1,17 +1,17 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import BootstrapClient from '../components/BootstrapClient';
-import { Geist, Geist_Mono } from "next/font/google";
+// import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
+// const geistSans = Geist({
+//   variable: "--font-geist-sans",
+//   subsets: ["latin"],
+// });
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+// const geistMono = Geist_Mono({
+//   variable: "--font-geist-mono",
+//   subsets: ["latin"],
+// });
 
 export const metadata = {
   title: "Alex Lee Sample React App",
@@ -21,9 +21,8 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      {/* <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}> */}
+      <body>
         <BootstrapClient />
         {children}
       </body>
